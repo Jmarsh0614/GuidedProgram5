b4e73f6e0029ecc3faa5fef245a5b184bcd10222
Joshua Marshall <jmarsh0614@gmail.com>
2017-03-05 08:39:16 -0500

    Coding completed

--- GuidedProgram5/frmGuidedProgram5.Designer.vb
+++ GuidedProgram5/frmGuidedProgram5.Designer.vb
@@ -22,9 +22,186 @@ Partial Class frmGuidedProgram5
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.lblSquareFeet = New System.Windows.Forms.Label()
+        Me.txtFootage = New System.Windows.Forms.TextBox()
+        Me.grpFloorType = New System.Windows.Forms.GroupBox()
+        Me.radHardwood = New System.Windows.Forms.RadioButton()
+        Me.radCarpet = New System.Windows.Forms.RadioButton()
+        Me.radTile = New System.Windows.Forms.RadioButton()
+        Me.lblCost = New System.Windows.Forms.Label()
+        Me.lblCostEstimate = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.picFlooring = New System.Windows.Forms.PictureBox()
+        Me.grpFloorType.SuspendLayout()
+        CType(Me.picFlooring, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("Goudy Old Style", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.Location = New System.Drawing.Point(58, 26)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(288, 32)
+        Me.lblHeading.TabIndex = 0
+        Me.lblHeading.Text = "Flooring Cost Calculator"
+        '
+        'lblSquareFeet
+        '
+        Me.lblSquareFeet.AutoSize = True
+        Me.lblSquareFeet.Font = New System.Drawing.Font("Goudy Old Style", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSquareFeet.Location = New System.Drawing.Point(99, 82)
+        Me.lblSquareFeet.Name = "lblSquareFeet"
+        Me.lblSquareFeet.Size = New System.Drawing.Size(149, 25)
+        Me.lblSquareFeet.TabIndex = 1
+        Me.lblSquareFeet.Text = "Square Footage:"
+        '
+        'txtFootage
+        '
+        Me.txtFootage.Font = New System.Drawing.Font("Goudy Old Style", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtFootage.Location = New System.Drawing.Point(246, 82)
+        Me.txtFootage.Name = "txtFootage"
+        Me.txtFootage.Size = New System.Drawing.Size(59, 30)
+        Me.txtFootage.TabIndex = 2
+        Me.txtFootage.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'grpFloorType
+        '
+        Me.grpFloorType.BackColor = System.Drawing.Color.White
+        Me.grpFloorType.Controls.Add(Me.radHardwood)
+        Me.grpFloorType.Controls.Add(Me.radCarpet)
+        Me.grpFloorType.Controls.Add(Me.radTile)
+        Me.grpFloorType.Font = New System.Drawing.Font("Goudy Old Style", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.grpFloorType.Location = New System.Drawing.Point(104, 132)
+        Me.grpFloorType.Name = "grpFloorType"
+        Me.grpFloorType.Size = New System.Drawing.Size(175, 105)
+        Me.grpFloorType.TabIndex = 3
+        Me.grpFloorType.TabStop = False
+        Me.grpFloorType.Text = "Flooring Type"
+        '
+        'radHardwood
+        '
+        Me.radHardwood.AutoSize = True
+        Me.radHardwood.Location = New System.Drawing.Point(8, 75)
+        Me.radHardwood.Name = "radHardwood"
+        Me.radHardwood.Size = New System.Drawing.Size(110, 27)
+        Me.radHardwood.TabIndex = 2
+        Me.radHardwood.Text = "Hardwood"
+        Me.radHardwood.UseVisualStyleBackColor = True
+        '
+        'radCarpet
+        '
+        Me.radCarpet.AutoSize = True
+        Me.radCarpet.Location = New System.Drawing.Point(8, 48)
+        Me.radCarpet.Name = "radCarpet"
+        Me.radCarpet.Size = New System.Drawing.Size(80, 27)
+        Me.radCarpet.TabIndex = 1
+        Me.radCarpet.Text = "Carpet"
+        Me.radCarpet.UseVisualStyleBackColor = True
+        '
+        'radTile
+        '
+        Me.radTile.AutoSize = True
+        Me.radTile.Checked = True
+        Me.radTile.Location = New System.Drawing.Point(7, 21)
+        Me.radTile.Name = "radTile"
+        Me.radTile.Size = New System.Drawing.Size(58, 27)
+        Me.radTile.TabIndex = 0
+        Me.radTile.TabStop = True
+        Me.radTile.Text = "Tile"
+        Me.radTile.UseVisualStyleBackColor = True
+        '
+        'lblCost
+        '
+        Me.lblCost.AutoSize = True
+        Me.lblCost.Font = New System.Drawing.Font("Goudy Old Style", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCost.Location = New System.Drawing.Point(56, 290)
+        Me.lblCost.Name = "lblCost"
+        Me.lblCost.Size = New System.Drawing.Size(136, 25)
+        Me.lblCost.TabIndex = 4
+        Me.lblCost.Text = "Cost Estimate:"
+        '
+        'lblCostEstimate
+        '
+        Me.lblCostEstimate.AutoSize = True
+        Me.lblCostEstimate.Font = New System.Drawing.Font("Goudy Old Style", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCostEstimate.Location = New System.Drawing.Point(211, 290)
+        Me.lblCostEstimate.Name = "lblCostEstimate"
+        Me.lblCostEstimate.Size = New System.Drawing.Size(94, 25)
+        Me.lblCostEstimate.TabIndex = 5
+        Me.lblCostEstimate.Text = "$0000.00"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.AutoSize = True
+        Me.btnCalculate.BackColor = System.Drawing.Color.White
+        Me.btnCalculate.Font = New System.Drawing.Font("Goudy Old Style", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(41, 337)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(116, 33)
+        Me.btnCalculate.TabIndex = 6
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.AutoSize = True
+        Me.btnClear.BackColor = System.Drawing.Color.White
+        Me.btnClear.Font = New System.Drawing.Font("Goudy Old Style", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(216, 337)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(116, 33)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'picFlooring
+        '
+        Me.picFlooring.Image = Global.GuidedProgram5.My.Resources.Resources.Flooring
+        Me.picFlooring.Location = New System.Drawing.Point(1, 376)
+        Me.picFlooring.Name = "picFlooring"
+        Me.picFlooring.Size = New System.Drawing.Size(400, 150)
+        Me.picFlooring.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picFlooring.TabIndex = 8
+        Me.picFlooring.TabStop = False
+        '
+        'frmGuidedProgram5
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.Color.Ivory
+        Me.ClientSize = New System.Drawing.Size(404, 521)
+        Me.Controls.Add(Me.picFlooring)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblCostEstimate)
+        Me.Controls.Add(Me.lblCost)
+        Me.Controls.Add(Me.grpFloorType)
+        Me.Controls.Add(Me.txtFootage)
+        Me.Controls.Add(Me.lblSquareFeet)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Name = "frmGuidedProgram5"
+        Me.Text = "                                            Flooring Cost"
+        Me.grpFloorType.ResumeLayout(False)
+        Me.grpFloorType.PerformLayout()
+        CType(Me.picFlooring, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents lblSquareFeet As Label
+    Friend WithEvents txtFootage As TextBox
+    Friend WithEvents grpFloorType As GroupBox
+    Friend WithEvents radHardwood As RadioButton
+    Friend WithEvents radCarpet As RadioButton
+    Friend WithEvents radTile As RadioButton
+    Friend WithEvents lblCost As Label
+    Friend WithEvents lblCostEstimate As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents picFlooring As PictureBox
 End Class
--- GuidedProgram5/frmGuidedProgram5.vb
+++ GuidedProgram5/frmGuidedProgram5.vb
@@ -1,3 +1,78 @@
-﻿Public Class frmGuidedProgram5
+﻿'Program Name : Flooring Cost Calculator Application
+'Author : Joshua Marshall
+'Date: March 5th, 2017
+'Purpose: THis Windows Application computes the estimated cost 
+'         of flooring based on the number of square feet and the 
+'         following cost per square foot: Tile - $4.49 per square foot; 
+'         Carpet - $4.99 per square foot: Hardwood - $7.49 per square foot.
 
+Option Strict On
+
+
+Public Class frmGuidedProgram5
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'The btnCaclulate event handler calculates the estimated cost of flooring
+        'based on the square foootage and the flooring type
+
+        'Declaration Section
+        Dim decFootage As Decimal
+        Dim decCostPerSquareFoot As Decimal
+        Dim decCostEstimate As Decimal
+        Dim decTileCost As Decimal = 4.49D
+        Dim decCarpetCost As Decimal = 4.99D
+        Dim decHardwoodCost As Decimal = 7.49D
+
+        'Did user enter a numeric value?
+        'If user enters a valued that is numeric, convert it to a decimal and then set decFootage equal to the value
+        If IsNumeric(txtFootage.Text) Then
+            decFootage = Convert.ToDecimal(txtFootage.Text)
+
+
+            'Is Square Footage Greater than Zero?
+            If decFootage > 0 Then
+                'Determine cost per square foot
+                If radTile.Checked Then
+                    decCostPerSquareFoot = decTileCost
+                ElseIf radCarpet.Checked Then
+                    decCostPerSquareFoot = decCarpetCost
+                ElseIf radHardwood.Checked Then
+                    decCostPerSquareFoot = decHardwoodCost
+                End If
+
+                'Caclulate and display the cost Estimate
+                decCostEstimate = decFootage * decCostPerSquareFoot
+                lblCostEstimate.Text = decCostEstimate.ToString("C")
+            Else
+                'Display error message if user entered a negative value
+                MsgBox("You entered " & decFootage.ToString() & ". Enter a Postive Number", , "Input Error")
+                txtFootage.Text = ""
+                txtFootage.Focus()
+
+
+            End If
+        Else
+            'Display error message if user entered a Non-Numeric Value
+            MsgBox("Enther the Square Footage of Flooring", , "Input Error")
+
+        End If
+
+
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtFootage.Clear()
+        lblCostEstimate.Text = ""
+        radTile.Checked = True
+        radCarpet.Checked = False
+        radHardwood.Checked = False
+        txtFootage.Focus()
+    End Sub
+
+    Private Sub frmGuidedProgram5_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtFootage.Focus()
+        lblCostEstimate.Text = ""
+
+
+    End Sub
 End Class
